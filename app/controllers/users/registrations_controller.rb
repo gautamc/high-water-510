2677c9bd5cc9ef3bdb3a23786804e0830a0c9f89
gautam <gautam.chekuri@gmail.com>
2011-02-25 07:31:56 +0530

    Account setting ajaxed action

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,23 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  skip_before_filter :require_no_authentication
+  skip_before_filter :require_no_authentication, :only => [:new, :create]
 
   def new
     @user = User.new
     @user.site = Site.new
+  end
+
+  def update
+    @user = current_user
+    @user.attributes = params[:user]
+    respond_to do |format|
+      if @user.save
+        format.html { render :partial => 'edit' }
+        format.js
+      else
+        format.html { render :partial => 'edit' }
+        format.js
+      end
+    end
   end
   
 end

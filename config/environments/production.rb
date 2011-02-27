d899896696f636c53eb38fbd43d1007dec8ca29f
gautam <gautam.chekuri@gmail.com>
2011-02-28 01:16:48 +0530

    mailer settings updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,12 +38,11 @@ Stark::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => 'high-water-510.heroku.com' }
-  ### ActionMailer Config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  
+ 
   # Enable threaded mode
   # config.threadsafe!
 
